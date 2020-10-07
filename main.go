c7c740ed2d53f02aff51005f2a5a101c010206c9
Sonika Sarangi <sonika.sa96@gmail.com>
2020-10-07 16:09:09 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+// [START container_hello_app]
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// register hello function to handle all requests
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	// use PORT environment variable, or default to 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// start the web server on port and accept requests
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	host, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello, This is CSE 5333 course.\n")
+	fmt.Fprintf(w,"Lab 3- Deploying containerized web application on GKE\n")
+	fmt.Fprintf(w,"Date : 10/07/2020\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Hostname: %s\n", host)
+}
+
+// [END container_hello_app]
